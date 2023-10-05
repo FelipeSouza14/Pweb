0f39fbabd1af00997f0c6e69e9816a26efcae277
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-10-04 21:57:47 -0300

    ajeitando o script

--- Unidade II/react_example/script.js
+++ Unidade II/react_example/script.js
@@ -1,5 +1,15 @@
 ReactDOM.render(
-  React.createElement("h1", null, "React!"),
+  React.createElement(
+    "h1",
+    { id: "my-heading" },
+    React.createElement(
+      "span",
+      null,
+      "React ",
+      React.createElement("em", null, "é muito")
+    ),
+    " massa!"
+  ),
 
   document.getElementById("app")
 );
