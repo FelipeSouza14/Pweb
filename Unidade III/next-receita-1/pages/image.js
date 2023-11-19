efab370ff27c4a7d5a23d3840676defc1ef14c5a
FelipeSouza14 <felipesouzabenicio@gmail.com>
2023-11-19 17:57:57 -0300

    add o js no image.js

---
+++ Unidade III/next-receita-1/pages/image.js
@@ -0,0 +1,7 @@
+import styles from "./styles/image.module.css";
+
+export function HomeImage() {
+    return(
+                <img src={"/assets/subnautica-image.png"} className={styles.imageStyle}/>
+    )
+}
